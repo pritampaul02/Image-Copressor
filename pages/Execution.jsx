cbc4dad6988a96567174d94d07235f159bab1719
Pritam <123.paulpritam@gmail.com>
2024-04-05 19:52:31 +0530

    Some Changes

--- pages/Execution.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Execution = () => {
-  return (
-    <div> Welcome </div>
-  )
-}
-
-export default Execution
---
+++ src/pages/Execution.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Execution = () => {
+  return (
+    <div> Welcome </div>
+  )
+}
+
+export default Execution
