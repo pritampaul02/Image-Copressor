e7763c4731d940053c4e5f0faced2d45c7f30f82
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-04-22 12:13:25 +0530

    edit login and error pages, create feedbacksection- Ahin

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Download from "./pages/Download";
 import "./style/global.css";
 import Error from "./pages/Error";
 import Login from "./pages/Login";
+import Feedback from "./pages/Feedback";
+import Profile from "./pages/Profile";
 
 const App = () => {
     return (
@@ -23,6 +25,8 @@ const App = () => {
                 <Route path="/uploaded-images" element={<Download />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/*" element={<Error />} />
+                <Route path="/feedback" element={<Feedback />} />
+                <Route path="/profile" element={<Profile/>} />
             </Routes>
 
             <Footer />
