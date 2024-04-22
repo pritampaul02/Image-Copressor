c4b67e9a75c996a1781d64f14a1a5003d66e9659
saniyaj mallik <saniyajmallik12@gmail.com>
2024-04-22 20:32:23 +0530

    patch update login register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,75 +1,71 @@
-
 import { Link, useLocation } from "react-router-dom";
 
 import profile from "../media/image.png";
 
 import "../style/navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
-    const activeLocation = useLocation().pathname;
+	const activeLocation = useLocation().pathname;
+	const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <header className="navber">
-            <Link to="/" className="logo">
-                Logo
-                
-            </Link>
-            {/* <div className="menu"> */}
-            <div className="menuContainer">
-                <Link
-                    to="/"
-                    className={activeLocation === "/" ? "menuActive" : ""}
-                >
-                    Home
-                    <span></span>
-                </Link>
-                <Link
-                    to="/about"
-                    className={activeLocation === "/about" ? "menuActive" : ""}
-                >
-                    About
-                    <span></span>
-                </Link>
-                <Link
-                    to="/uploaded-images"
-                    className={
-                        activeLocation === "/uploaded-images"
-                            ? "menuActive"
-                            : ""
-                    }
-                >
-                    Uploaded Image
-                    <span></span>
-                </Link>
-                <Link
-                    to="/login"
-                    className={activeLocation === "/login" ? "menuActive" : ""}
-                >
-                    Login
-                    <span></span>
-                </Link>
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+		console.log(isOpen);
+	};
 
-                <Link className="profileContainer" to="/">
-                    <img src={profile} alt="" />
-                </Link>
+	return (
+		<header className='navber'>
+			<Link to='/' className='logo'>
+				Logo
+			</Link>
+			{/* <div className="menu"> */}
+			<div className='menuContainer'>
+				<Link to='/' className={activeLocation === "/" ? "menuActive" : ""}>
+					Home
+					<span></span>
+				</Link>
+				<Link
+					to='/about'
+					className={activeLocation === "/about" ? "menuActive" : ""}
+				>
+					About
+					<span></span>
+				</Link>
+				<Link
+					to='/uploaded-images'
+					className={activeLocation === "/uploaded-images" ? "menuActive" : ""}
+				>
+					Uploaded Image
+					<span></span>
+				</Link>
+				<Link
+					to='/login'
+					className={activeLocation === "/login" ? "menuActive" : ""}
+				>
+					Login
+					<span></span>
+				</Link>
 
-                
-                <ul className="menu">
-                    <li className="menu-item">
-                      <Link
-                    to="/profile"
-                    className={activeLocation === "/login" ? "menuActive" : ""}
-                >
-                     Go to profile
-                </Link>
-                </li>
-                    <li className="menu-item">Logout</li>
-                </ul>
-                
-            </div>
-            {/* </div> */}
-        </header>
-    );
+				<Link onClick={toggleMenu} className='profileContainer' to='/'>
+					<img src={profile} alt='' />
+				</Link>
+
+				<ul className={`menu  ${isOpen ? "show" : "hidden"}`}>
+					<li className='menu-item'>
+						<Link
+							to='/profile'
+							className={activeLocation === "/login" ? "menuActive" : ""}
+						>
+							Go to profile
+						</Link>
+					</li>
+					<li className='menu-item'>Logout</li>
+				</ul>
+			</div>
+			{/* </div> */}
+		</header>
+	);
 };
 
 export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,33 +3,71 @@ import { GoogleLogin } from "@react-oauth/google";
 import login from "../media/login.png";
 import google from "../media/google.png";
 import "../style/login.css";
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-    return (
-        <>
-            <div className="login_page">
-                <div className="login_img">
-                    <div className="img" id ="googlelogin">
-                        <div className="login">
-                            <h2>Quick Compressor </h2>
-                            <p>Compress your image quickly </p>
-                            <GoogleLogin className="button"
-                                onSuccess={(credentialResponse) => {
-                                    console.log(credentialResponse);
-                                }}
-                                onError={() => {
-                                    console.log("Login Failed");
-                                }}
-                            />
-                        </div>
-                    </div>
-                    <div className="img" id ="benifit">
-                        <h2>Login Benifits</h2>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [profilePic, setProfilePic] = useState("");
+
+	const register = async () => {
+		try {
+            console.log('registre');
+			const res = await axios.post(
+				"http://localhost:8080/api/user/google/signup",
+				{
+					name,
+					email,
+					profilePic,
+				}
+			);
+			console.log(res.data);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	const handleSignup = (credentialResponse) => {
+		let decoded = jwtDecode(credentialResponse.credential);
+		let name2 = decoded.given_name + " " + decoded.family_name;
+
+
+        setName(name2)
+        setEmail(decoded.email)
+        setProfilePic(decoded.picture)
+        register()
+
+
+		// register()
+		console.log();
+	};
+
+	return (
+		<>
+			<div className='login_page'>
+				<div className='login_img'>
+					<div className='img' id='googlelogin'>
+						<div className='login'>
+							<h2>Quick Compressor </h2>
+							<p>Compress your image quickly </p>
+							<GoogleLogin
+								className='button'
+								onSuccess={handleSignup}
+								onError={() => {
+									console.log("Login Failed");
+								}}
+							/>
+						</div>
+					</div>
+					<div className='img' id='benifit'>
+						<h2>Login Benifits</h2>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Login;
