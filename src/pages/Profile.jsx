a752a91e4eac0b7bbbe1e609ebb61fbbe1007ebc
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-04-25 13:18:48 +0530

    create profilesection and edit login section- Ahin

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/slices/userSlice";
+import "../style/profile.css";
 
 const Profile = () => {
 	const { user } = useSelector(selectUser);
@@ -9,10 +10,21 @@ const Profile = () => {
   }, [user])
   
 	return (
-		<div>
-			<h1>profile</h1>
-			<h1>{user?.name}</h1>
-		</div>
+		<>
+		   {/* <div> */}
+		   {/* <h1>profile</h1> */}
+		   {/* <h1>{user?.name}</h1> */}
+		   {/* </div> */}
+
+		  <div className="profile">
+		    <div className="profile_box">
+		       <div className="profile_image">
+				 </div>
+			
+			 </div>	
+		  </div>
+		    
+		</>
 	);
 };
 
