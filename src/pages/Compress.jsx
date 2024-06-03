0c8ed26f3a17d7321e080c630dd762ed520457b5
saniyaj mallik <saniyajmallik12@gmail.com>
2024-06-03 20:07:04 +0530

    compress complete

--- src/pages/Compress.jsx
+++ src/pages/Compress.jsx
@@ -15,156 +15,189 @@ import { saveImage, selectImage } from "../redux/slices/imageSlice";
 import toast from "react-hot-toast";
 
 const Compress = () => {
-    const [imageSize, setImageSize] = useState("");
-    const [uploadedImage, setUploadedImage] = useState(addImage);
-    const [compressedImage, setCompressedImage] = useState(noImage);
+	// const [imageSize, setImageSize] = useState("");
+	// const [uploadedImage, setUploadedImage] = useState(addImage);
+	// const [compressedImage, setCompressedImage] = useState(noImage);
+	// const [imageData, setImageData] = useState("");
+	const { status, selectedImage } = useSelector(selectImage);
 
-    const [imageData, setImageData] = useState("");
-    // const [imgPreview, setImgPreview] = useState('/vite.svg')
-    const { status, selectedImage } = useSelector(selectImage);
+	const [v, setV] = useState(null);
+	const [originalSize, setOriginalSize] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [percent, setPercentage] = useState(1);
+	const [CPSize, setCPSize] = useState(null);
+	const [resultImg, setResultImg] = useState();
+	const [imgPreview, setImgPreview] = useState(noImage);
 
-    useEffect(() => {
-        if (status.uploadStatus === "success") {
-            toast.success("Image Saved Successfully");
-        }
-    }, [status.uploadStatus]);
+	const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+	useEffect(() => {
+		if (status.uploadStatus === "success") {
+			toast.success("Image Saved Successfully");
+		}
+	}, [status.uploadStatus]);
 
-    const handleImageUpload = async (event) => {
-        const imageFile = event.target.files[0];
+	useEffect(() => {
+		if (v !== null) {
+			handleImageUpload(v);
+		}
+	}, [percent]);
 
-        const options = {
-            maxSizeMB: 1,
-            maxWidthOrHeight: 1920,
-            useWebWorker: true,
-        };
-        try {
-            const compressedFile = await imageCompression(imageFile, options);
-            console.log(
-                `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
-            ); // smaller than maxSizeMB
-            setImageSize(compressedFile.size / 1024 / 1024);
+	const setImageHandler = (e) => {
+		const image = e.target.files[0];
+		setV(image);
+		handleImageUpload(image);
+		const reader = new FileReader();
 
-            const reader = new FileReader();
+		reader.onload = function (e) {
+			setImgPreview(e.target.result);
+		};
+		reader.readAsDataURL(image);
+	};
 
-            reader.onload = function (e) {
-                // Set the result of the FileReader to the state variable
-                setImageData(e.target.result);
-                setUploadedImage(e.target.result);
-                setCompressedImage(e.target.result);
-                console.log(e.target.result);
-            };
+	const handleImageUpload = async (imageFile) => {
+		console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+		let originalSize = imageFile.size / 1024 / 1024;
+		setOriginalSize(originalSize.toFixed(3));
+		setLoading(true);
 
-            reader.readAsDataURL(compressedFile);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+		const options = {
+			maxSizeMB: (percent / 10).toFixed(1),
+			maxWidthOrHeight: 1920,
+			useWebWorker: true,
+		};
+		try {
+			const compressedFile = await imageCompression(imageFile, options);
+			setLoading(false);
 
-    const handleSaveImage = () => {
-        console.log(88989);
-        dispatch(saveImage(compressedImage));
-    };
+			console.log(
+				`compressedFile size ${compressedFile.size / 1024 / 1024} MB`
+			);
+			setCPSize((compressedFile.size / 1024 / 1024).toFixed(3));
 
-    return (
-        <section className="compressPage">
-            <div className="backgroundStyle"></div>
-            <div className="compressContainer">
-                <div className="beforeImage">
-                    <img
-                        src={uploadedImage}
-                        style={
-                            uploadedImage === addImage
-                                ? { width: "60px", height: "auto" }
-                                : {}
-                        }
-                        alt="Your Uploaded Image"
-                    />
-                </div>
-                <img src={spiral} alt="spiral" className="spiralSvg" />
-                <div className="afterImage">
-                    <img
-                        src={compressedImage}
-                        style={
-                            compressedImage === noImage
-                                ? { width: "60px", height: "auto" }
-                                : {}
-                        }
-                        alt="Your Uploaded Image"
-                    />
-                </div>
-            </div>
+			const reader = new FileReader();
 
-            <div className="compressActions">
-                <input
-                    type="file"
-                    onChange={handleImageUpload}
-                    id="imageUploadBtn"
-                    hidden
-                />
-                <label htmlFor="imageUploadBtn" className="image_uploader">
-                    Upload{" "}
-                    {/* <span>{uploadedImage !== addImage ? ` ${uploadedImage}` : ""}</span> */}
-                    <span>{imageSize}MB</span>
-                </label>
+			reader.onload = function (e) {
+				setResultImg(e.target.result);
+				setImgPreview(e.target.result);
+			};
+			reader.readAsDataURL(compressedFile);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-                <div className="rangeSlider">
-                    <p>min : 50kb</p>
-                    <input
-                        type="range"
-                        min={10}
-                        max={100}
-                        value={imageSize}
-                        onChange={(e) => setImageSize(e.target.value)}
-                        className="sizeChanger"
-                    />
-                    <p>{imageSize}</p>
-                </div>
+	
 
-                <div className="actions">
-                    <button
-                        href="/"
-                        download={true}
-                        className="btn-compress"
-                        title="Download"
-                    >
-                        {/* <p>Download</p> */}
-                        <IoCloudDownloadSharp />
-                    </button>
-                    <button
-                        onClick={handleSaveImage}
-                        className="btn-compress"
-                        title="Upload"
-                    >
-                        {/* <p>Upload</p> */}
-                        <IoCloudUploadSharp />
-                    </button>
-                    <button
-                        onClick={() => {
-                            selectedImage &&
-                                navigator.clipboard
-                                    .writeText(selectedImage?.url)
-                                    .then(() => {
-                                        toast.success("Url copied");
-                                    });
-                        }}
-                        className="btn-compress"
-                        title="Share"
-                    >
-                        {/* <p>Share</p> */}
-                        <IoIosShareAlt />
-                    </button>
-                    {/* <button
+	const handleSaveImage = () => {
+		console.log(88989);
+		dispatch(saveImage(resultImg));
+	};
+
+	return (
+		<section className='compressPage'>
+			<div className='backgroundStyle'></div>
+			<div className='compressContainer'>
+				<div className='beforeImage'>
+					<img
+						src={imgPreview}
+						// style={
+						// 	uploadedImage === addImage
+						// 		? { width: "60px", height: "auto" }
+						// 		: {}
+						// }
+						alt='Your Uploaded Image'
+					/>
+				</div>
+				<img src={spiral} alt='spiral' className='spiralSvg' />
+				<div className='afterImage'>
+					{/* <img
+						src={compressedImage}
+						style={
+							compressedImage === noImage
+								? { width: "60px", height: "auto" }
+								: {}
+						}
+						alt='Your Uploaded Image'
+					/> */}
+
+					{loading === true ? (
+						<h2>Loading</h2>
+					) : (
+						<img className='compress-img' src={resultImg} alt='hi' />
+					)}
+				</div>
+			</div>
+
+			<div className='compressActions'>
+				<input
+					type='file'
+					onChange={setImageHandler}
+					id='imageUploadBtn'
+					hidden
+				/>
+				<label htmlFor='imageUploadBtn' className='image_uploader'>
+					Upload{" "}
+				</label>
+
+				<div className='rangeSlider'>
+					<p>Value: {percent}%</p>
+					<input
+						type='range'
+						min='1'
+						max='100'
+						value={percent}
+						onChange={(e) => setPercentage(e.target.value)}
+						className='sizeChanger'
+					/>
+					<p>original Size: {originalSize}mb</p>
+					<p>compress Size: {CPSize}mb</p>
+				</div>
+
+				<div className='actions'>
+					<a
+						href={resultImg}
+						download='compressed_image.jpg'
+						className='btn-compress'
+					>
+						{/* <p>Download</p> */}
+						<IoCloudDownloadSharp />
+					</a>
+					<button
+						onClick={handleSaveImage}
+						className='btn-compress'
+						title='Upload'
+					>
+						{/* <p>Upload</p> */}
+						<IoCloudUploadSharp />
+					</button>
+
+					{selectedImage?.url != undefined && (
+						<button
+							onClick={() => {
+								selectedImage &&
+									navigator.clipboard.writeText(selectedImage?.url).then(() => {
+										toast.success("Url copied");
+									});
+							}}
+							className='btn-compress'
+							title='Share'
+						>
+							{/* <p>Share</p> */}
+							<IoIosShareAlt />
+						</button>
+					)}
+
+					{/* <button
                         type="submit"
                         className="btn-compress btnCompress-primary"
                     >
                         Compress
                     </button> */}
-                </div>
-            </div>
-        </section>
-    );
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Compress;
